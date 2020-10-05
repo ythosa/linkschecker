d29743304bf2cf88266be86fa328a8befed85db3
ythosa <vasus714@yandex.ru>
2020-10-05 21:05:08 +0300

    Links extracter added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ythosa/linkschecker
+
+go 1.14
+
+require golang.org/x/net v0.0.0-20201002202402-0a1ea396d57c // indirect
--- src/links.go
+++ src/links.go
@@ -2,36 +2,70 @@ package main
 
 import (
 	"fmt"
-)
+	"log"
+	"net/http"
 
-// BreadthFirst ...
-func BreadthFirst(f func(item string) []string, worklist []string) {
-	seen := make(map[string]bool)
-	for len(worklist) > 0 {
-		items := worklist
-		worklist = nil
-		for _, item := range items {
-			if !seen[item] {
-				seen[item] = true
-				worklist = append(worklist, f(item)...)
-			}
-		}
-	}
-}
+	"golang.org/x/net/html"
+)
 
 // Crawl ...
 func Crawl(url string) []string {
 	fmt.Println(url)
-	//list, err := Extract(url)
-	//if err != nil {
-	//	log.Print(err)
-	//}
-	//
-	//return list
+	list, err := Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
 
-	return []string{}
+	return list
 }
 
-//func Extract(url string) []string {
-//
-//}
+func Extract(url string) ([]string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		fmt.Printf("Link with URL {%s} responds status code: %d", url, response.StatusCode)
+	}
+
+	doc, err := html.Parse(response.Body)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+
+				link, err := response.Request.URL.Parse(a.Val)
+				if err != nil {
+					fmt.Printf("Link with incorrect URL: %s", url)
+					continue
+				}
+
+				links = append(links, link.String())
+			}
+		}
+	}
+
+	ForEachNode(doc, visitNode)
+	return links, nil
+}
+
+func ForEachNode(node *html.Node, f func(n *html.Node)) {
+	if f != nil {
+		f(node)
+	}
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		ForEachNode(c, f)
+	}
+}
