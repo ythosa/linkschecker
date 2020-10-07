f4327cf75eb856fdded9be26f073960c58287175
ythosa <vasus714@yandex.ru>
2020-10-07 16:50:38 +0300

    Removed unused

--- src/links/errors.go
+++ src/links/errors.go
@@ -15,10 +15,6 @@ type invalidResponseTypeException struct {
     url string
 }
 
-type invalidURLException struct {
-    url string
-}
-
 func NewBadStatusCodeException(url string, statusCode int) error {
     return &badStatusCodeException{url: url, statusCode: statusCode}
 }
@@ -31,10 +27,6 @@ func NewInvalidResponseTypeException(url string) error {
     return &invalidResponseTypeException{url: url}
 }
 
-func NewInvalidURLException(url string) error {
-    return &invalidURLException{url: url}
-}
-
 func (e *badStatusCodeException) Error() string {
     return fmt.Sprintf("%s - bad status code response - %d", e.url, e.statusCode)
 }
@@ -45,8 +37,4 @@ func (e *unreachableSiteException) Error() string {
 
 func (e *invalidResponseTypeException) Error() string {
     return fmt.Sprintf("%s - invalid response body", e.url)
-}
-
-func (e *invalidURLException) Error() string {
-    return fmt.Sprintf("%s - invalid format", e.url)
 }
